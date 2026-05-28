df6356f2cb9b490a0c86daad303675e723bac159
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-05-28 09:24:37 +0200

    chore(auth) : add uix

---
+++ mwl-app/mwl-frontend/src/features/auth/components/Register.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+<template>
+  <v-container class="d-flex align-center justify-center" style="min-height: 100vh;">
+    <v-card width="400">
+      <h1 class="text-center mt-4">
+        My Whish List
+      </h1>
+      <v-img src="/logo.png" height="150"></v-img>
+      <v-card-title class="text-center">Inscription</v-card-title>
+      <v-card-text>
+        <v-text-field label="Nom" type="text" required></v-text-field> 
+        <v-text-field label="Email" type="email" required></v-text-field>
+        <v-text-field label="Password" type="password" required></v-text-field>
+        <v-btn color="primary" class="mt-2" block>S'enregistrer</v-btn>
+      </v-card-text>
+    </v-card>
+  </v-container> 
+</template>
--- mwl-app/mwl-frontend/src/router/index.ts
+++ mwl-app/mwl-frontend/src/router/index.ts
@@ -3,14 +3,19 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/features/auth/components/Login.vue'
+import Register from '@/features/auth/components/Register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/login',
       component: Login,
     },
+    {
+      path: '/register',
+      component: Register,
+    }
   ],
 })
 
